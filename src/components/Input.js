4f8f5e29bc082f96129cbb1cca5f0590b4da06ba
Hyeonmin Kwon <hyeonmeenoi@gmail.com>
2023-08-11 18:53:21 +0900

    1. Initial Commit

---
+++ src/components/Input.js
@@ -0,0 +1,114 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import propTypes from 'prop-types';
+import { forwardRef } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GRAY, PRIMARY } from '../colors';
+import { useState } from 'react';
+
+export const ReturnKeyTypes = {
+  DONE: 'done',
+  NEXT: 'next',
+};
+
+export const InputTypes = {
+  EMAIL: 'EMAIL',
+  PASSWORD: 'PASSWORD',
+};
+
+const InputTypeProps = {
+  EMAIL: {
+    title: 'EMAIL',
+    placeholder: 'your@email.com',
+    keyboardType: 'email-address',
+    secureTextEntry: false,
+    iconName: { active: 'email', inactive: 'email-outline' },
+  },
+  PASSWORD: {
+    title: 'PASSWORD',
+    placeholder: 'PASSWORD',
+    keyboardType: 'default',
+    secureTextEntry: true,
+    iconName: { active: 'lock', inactive: 'lock-outline' },
+  },
+};
+
+const Input = forwardRef(({ inputType, ...props }, ref) => {
+  const {
+    title,
+    placeholder,
+    keyboardType,
+    secureTextEntry,
+    iconName: { active, inactive },
+  } = InputTypeProps[inputType];
+  const { value } = props;
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View style={defaultStyles.container}>
+      <Text
+        style={[
+          defaultStyles.title,
+          { color: value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK },
+        ]}
+      >
+        {title}
+      </Text>
+      <View>
+        <TextInput
+          ref={ref}
+          {...props}
+          placeholder={placeholder}
+          keyboardType={keyboardType}
+          secureTextEntry={secureTextEntry}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          style={[
+            defaultStyles.input,
+            {
+              borderColor: value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK,
+              color: value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK,
+            },
+          ]}
+          textContentType="none"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <View style={defaultStyles.icon}>
+          <MaterialCommunityIcons
+            name={isFocused ? active : inactive}
+            size={24}
+            color={value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK}
+          />
+        </View>
+      </View>
+    </View>
+  );
+});
+
+Input.displayName = 'Input';
+
+Input.propTypes = {
+  inputType: propTypes.oneOf(Object.values(InputTypes)).isRequired,
+  value: propTypes.string.isRequired,
+};
+
+const defaultStyles = StyleSheet.create({
+  container: { width: '100%' },
+  title: { marginBottom: 4, fontWeight: '700' },
+  input: {
+    borderBottomWidth: 1,
+    borderRadius: 8,
+    height: 42,
+    paddingHorizontal: 10,
+    paddingLeft: 40,
+  },
+  icon: {
+    position: 'absolute',
+    left: 8,
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
+
+export default Input;
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,44 @@
+import { useNavigation } from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthRoutes } from '../navigations/routes';
+import Input, { ReturnKeyTypes, InputTypes } from '../components/Input';
+import { useState } from 'react';
+
+const SignInScreen = () => {
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text>Sign In</Text>
+      <Input
+        value={email}
+        onChangeText={(text) => setEmail(text.trim())}
+        inputType={InputTypes.EMAIL}
+        ReturnKeyTypes={ReturnKeyTypes.NEXT}
+      />
+      <Input
+        value={password}
+        onChangeText={(text) => setPassword(text.trim())}
+        inputType={InputTypes.PASSWORD}
+        ReturnKeyTypes={ReturnKeyTypes.DONE}
+      />
+      <Button
+        title="Sign Up"
+        onPress={() => navigation.navigate(AuthRoutes.SIGN_UP)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SignInScreen;
